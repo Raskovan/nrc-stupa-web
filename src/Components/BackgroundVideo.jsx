0e7c33e7e786e38d9111546677a3cc9262464efe
Raskovan <alexey.katalkin@gmail.com>
2021-04-04 21:16:23 -0400

    video dangerousHtml

--- src/Components/BackgroundVideo.jsx
+++ src/Components/BackgroundVideo.jsx
@@ -4,12 +4,36 @@ import NrcLogo from '../assets/stupa-logo.png'
 
 function BackgroundVideo() {
   const videoSource = './nrc_video.mp4'
+  const videoParentRef = React.useRef()
   return (
     <div>
-      <video autoPlay loop muted className={classes.Video} playsInline>
+      <div
+        ref={videoParentRef}
+        dangerouslySetInnerHTML={{
+          __html: `
+        <video
+          loop
+          muted
+          autoplay
+          playsInline
+          preload="metadata"
+          style="object-fit: cover; width: 100vw; height: 100vh; top: 0; left: 0;"
+        >
+        <source src="${videoSource}" type="video/mp4" />
+        </video>`
+        }}
+      />
+      {/* <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="metadata"
+        className={classes.Video}
+      >
         <source src={videoSource} type="video/mp4" />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
       <div className={classes.Content}>
         <img src={NrcLogo} width="120px" alt="NRC logo" />
         <div className={classes.SubContent}>
